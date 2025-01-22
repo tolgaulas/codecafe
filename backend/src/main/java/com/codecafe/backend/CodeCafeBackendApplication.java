0a03475551b4e8a72bcd844edb7171a11aee9b52
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-01-21 21:28:40 -0500

    Initialize backend with Spring Boot

---
+++ backend/src/main/java/com/codecafe/backend/CodeCafeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCafeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCafeBackendApplication.class, args);
+	}
+
+}
