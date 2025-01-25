ffcebf04f483b0993b7700852053a4f36fe83189
Andry Arthur <andry1arthur@gmail.com>
2025-01-25 01:32:04 -0500

    Adding Dependencies

--- backend/src/main/java/com/codecafe/backend/controller/WebSocketController.java
+++ backend/src/main/java/com/codecafe/backend/controller/WebSocketController.java
@@ -1,2 +1,15 @@
-package com.codecafe.backend.controller;public class WebSocketController {
+package com.codecafe.backend.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public String sendHelloMessage() {
+        return "Hello World";
+    }
 }
