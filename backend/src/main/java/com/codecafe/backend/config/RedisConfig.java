0639491f4478c21dc1f1efd6e4ad86dd05ac5197
Andry Arthur <andry1arthur@gmail.com>
2025-02-01 22:44:45 -0500

    Fixing Redis serializable issue

--- backend/src/main/java/com/codecafe/backend/config/RedisConfig.java
+++ backend/src/main/java/com/codecafe/backend/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -13,8 +14,13 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+
+        // Use StringRedisSerializer for keys
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
+
+        // Use GenericJackson2JsonRedisSerializer for values
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         return template;
     }
 }
