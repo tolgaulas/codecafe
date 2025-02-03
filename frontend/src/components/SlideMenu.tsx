ae1808873d8070b7c85ee489fada6710dddf7903
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-02-02 23:27:02 -0500

    Edit Button Radius

--- frontend/src/components/SlideMenu.tsx
+++ frontend/src/components/SlideMenu.tsx
@@ -38,7 +38,7 @@ const SlideMenu = () => {
         <div className="space-y-6 py-4">
           {/* Start New Code Space Button */}
           <div className="px-2">
-            <button className="mt-8 flex items-center gap-2 w-full py-2 px-2 rounded-lg transition-colors duration-200 bg-transparent hover:bg-neutral-900 active:bg-stone-950 text-stone-500 hover:text-stone-400">
+            <button className="mt-8 flex items-center gap-2 w-full py-2 px-2 rounded-md transition-colors duration-200 bg-transparent hover:bg-neutral-900 active:bg-stone-950 text-stone-500 hover:text-stone-400">
               <IoIosAddCircleOutline className="w-5 h-5" />
               <span className="text-sm">New</span>
             </button>
