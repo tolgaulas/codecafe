9bd432b2e14d67f497188793f419cb763fe57e18
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-03-27 03:50:32 -0400

    ADD: Constants Folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,18 +24,7 @@ import {
 import { User } from "./types/user";
 import { CodeExecutionRequest, CodeExecutionResponse } from "./types/code";
 import { CursorData } from "./types/cursorData";
-
-const languageVersions = {
-  typescript: { version: "1.32.3", name: "TypeScript" },
-  javascript: { version: "1.32.3", name: "JavaScript" },
-  python: { version: "3.10.0", name: "Python" },
-  java: { version: "15.0.2", name: "Java" },
-  c: { version: "10.2.0", name: "C" },
-  cplusplus: { version: "10.2.0", name: "C++" },
-  go: { version: "1.16.2", name: "Go" },
-  rust: { version: "1.68.2", name: "Rust" },
-  ruby: { version: "3.0.1", name: "Ruby" },
-};
+import { languageVersions } from "./types/languages";
 
 const App = () => {
   const [code, setCode] = useState<string>("// Hello there");
