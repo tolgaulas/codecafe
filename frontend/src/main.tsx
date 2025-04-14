4b10235e1162fd810e220dda3013142268b47eb7
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-04-13 21:26:43 -0400

    Remove Old UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import CodeEditorUI from "./CodeEditorUI.tsx";
+// import App from "./App.tsx"; // Remove this line
+import App from "./App.tsx"; // Change 'CodeEditorUI' to 'App' and path to './App.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CodeEditorUI />
+    <App /> // Change 'CodeEditorUI' to 'App'
   </StrictMode>
 );
