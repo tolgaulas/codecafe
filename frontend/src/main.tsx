d19cc92b923bc48b4c90154dd69c6bc71f307a32
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-04-16 23:23:13 -0400

    Update frontend styles and fix Safari overscroll

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from "./App.tsx"; // Remove this line
-import App from "./App.tsx"; // Change 'CodeEditorUI' to 'App' and path to './App.tsx'
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App /> // Change 'CodeEditorUI' to 'App'
+    <App />
   </StrictMode>
 );
